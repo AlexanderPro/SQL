986f23a56e031ddaec9aba4aa5decb29b6d0462e
AlexanderPro <illarionov.pro@gmail.com>
2017-10-01 12:59:52 +0300

    Gets a list of databases and their sizes.

---
+++ PostgreSQL/GetListOfDatabasesAndTheirSizes.sql
@@ -0,0 +1,15 @@
+--SELECT datname, pg_size_pretty(pg_database_size(datname))
+--FROM pg_database
+--ORDER BY pg_database_size(datname) DESC
+
+SELECT d.datname AS Name, pg_catalog.pg_get_userbyid(d.datdba) AS Owner,
+  CASE WHEN pg_catalog.has_database_privilege(d.datname, 'CONNECT')
+    THEN pg_catalog.pg_size_pretty(pg_catalog.pg_database_size(d.datname)) 
+    ELSE 'No Access' 
+  END AS SIZE 
+FROM pg_catalog.pg_database d 
+ORDER BY 
+  CASE WHEN pg_catalog.has_database_privilege(d.datname, 'CONNECT') 
+    THEN pg_catalog.pg_database_size(d.datname)
+    ELSE NULL 
+  END DESC
