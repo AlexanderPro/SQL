d4c5d0aa507e7487a9cd80e9ca0c0909bcd3bf81
AlexanderPro <illarionov.pro@gmail.com>
2018-11-21 12:51:16 +0300

    Gets a list of functions in a scheme.

---
+++ PostgreSQL/GetListOfFunctionsInScheme.sql
@@ -0,0 +1,8 @@
+SELECT n.nspname AS schema_name, 
+       p.proname AS function_name, 
+       pg_get_function_arguments(p.oid) AS args, 
+       pg_get_functiondef(p.oid) AS func_def
+FROM   pg_proc p
+JOIN   pg_namespace n ON n.oid = p.pronamespace
+WHERE  NOT p.proisagg
+AND    n.nspname IN ('public')
