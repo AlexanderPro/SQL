c685fc50549375ef27904b41199bb4798faee82c
AlexanderPro <illarionov.pro@gmail.com>
2018-11-28 09:14:51 +0300

    Adds a column name.

--- PostgreSQL/GetListOfIndexesAndTheirSizes.sql
+++ PostgreSQL/GetListOfIndexesAndTheirSizes.sql
@@ -1,5 +1,6 @@
-SELECT idx.relname as table,
-       idx.indexrelname as index,
+SELECT idx.relname as table_name,
+       idx.indexrelname as index_name,
+       atr.attname as column_name,
        pg_size_pretty( pg_relation_size( cls.oid ) ) AS table_size,
        pg_size_pretty( pg_relation_size( idx.indexrelid ) ) as index_size,
        pg_size_pretty( pg_total_relation_size( cls.oid ) ) AS total_size,
@@ -10,9 +11,12 @@ SELECT idx.relname as table,
        idx.idx_tup_fetch as fetched
   FROM pg_stat_user_indexes idx,
        pg_class cls,
-       pg_index pgi
+       pg_index pgi,
+       pg_attribute atr
  WHERE cls.relname = idx.relname
    AND idx.indexrelid = pgi.indexrelid
+   AND atr.attrelid = cls.oid
+   AND atr.attnum = ANY(pgi.indkey)
    AND pgi.indisunique is not true
    AND pgi.indisprimary is not true
    AND idx.indexrelname not ilike '%slony%'
