b6bf1ce94905dbf23e9626f15780dc84a6be5d9e
AlexanderPro <illarionov.pro@gmail.com>
2022-04-22 16:22:35 +0300

    Show opened and allowed number connections

---
+++ PostgreSQL/ShowOpenedAllowedConnections.sql
@@ -0,0 +1,5 @@
+select max_conn, used, res_for_super, max_conn-used-res_for_super as res_for_normal
+from
+  (select count(*) used from pg_stat_activity) t1,
+  (select setting::int res_for_super from pg_settings where name='superuser_reserved_connections') t2,
+  (select setting::int max_conn from pg_settings where name='max_connections') t3;
